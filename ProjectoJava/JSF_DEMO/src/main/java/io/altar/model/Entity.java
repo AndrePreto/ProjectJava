e39909c547595d3bea75ae94d34f586d9617f56c
AndrePreto <a.preto@campus.fct.unl.pt>
2017-06-19 13:20:39 +0100

    fiz cenas

---
+++ ProjectoJava/JSF_DEMO/src/main/java/io/altar/model/Entity.java
@@ -0,0 +1,16 @@
+package io.altar.model;
+
+public class Entity{
+	
+	private int Id;
+	
+	public int getId() {
+		return Id;
+	}
+	
+	public void setId(int Id) {
+		this.Id = Id;
+	}
+	
+	public Entity() {}
+}
