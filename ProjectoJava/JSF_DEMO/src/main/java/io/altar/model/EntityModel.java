0f15d802f93c16131946dc58b52068917d6caafe
AndrePreto <a.preto@campus.fct.unl.pt>
2017-07-06 13:22:10 +0100

    aula jee 13

--- ProjectoJava/JSF_DEMO/src/main/java/io/altar/model/EntityModel.java
+++ ProjectoJava/JSF_DEMO/src/main/java/io/altar/model/EntityModel.java
@@ -1,7 +1,14 @@
 package io.altar.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class EntityModel{
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int Id;
 	
 	public int getId() {
